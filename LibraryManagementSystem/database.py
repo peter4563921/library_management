9a64f2d96d0e6db52d6daf714519d263a2b96409
peter4563921 <sudhakarsudhakar79854@gmail.com>
2026-06-24 11:49:50 +0530

    Update database.py

--- LibraryManagementSystem/database.py
+++ LibraryManagementSystem/database.py
@@ -1,16 +1,21 @@
-import mysql.connector
+import sqlite3
 
 try:
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="library_db"
-    )
-
+    conn = sqlite3.connect("library.db")
     cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books (
+        book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        author TEXT,
+        quantity INTEGER
+    )
+    """)
+
+    conn.commit()
 
     print("Database Connected Successfully")
 
-except mysql.connector.Error as err:
+except Exception as err:
     print("Error:", err)
